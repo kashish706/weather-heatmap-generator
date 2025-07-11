876889307ab61c1956bb11b510be26e7a020c9ee
kashish706 <kashishsrivastava276@gmail.com>
2025-07-11 15:39:51 +0530

    spaces

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-// Function to get color name based on temperature
 string getColor(int temp) {
     if (temp >= 40) return "Red";
     else if (temp >= 30) return "Orange";
@@ -10,17 +8,12 @@ string getColor(int temp) {
     else if (temp >= 10) return "Green";
     else return "Blue";
 }
-
 int main() {
     int rows, cols;
-
     cout << "Enter number of rows and columns: ";
     cin >> rows >> cols;
-
     // Create 2D vector to store temperature values
     vector<vector<int>> tempGrid(rows, vector<int>(cols));
-
-    // Input temperatures
     cout << "Enter temperatures:\n";
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
@@ -28,8 +21,6 @@ int main() {
             cin >> tempGrid[i][j];
         }
     }
-
-    // Display heatmap (color names)
     cout << "\nWeather Heatmap (Color-coded):\n";
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
@@ -37,6 +28,5 @@ int main() {
         }
         cout << "\n";
     }
-
     return 0;
 }
